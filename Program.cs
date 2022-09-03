4b223f945940dab2b9166722197107bb41b4b569
LevchenkoL <levchenko.levan@mail.ru>
2022-09-03 10:30:23 +0700

    .

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿//using System;
 
-namespace BaseLab1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+//int a, b;
+
+//Console.WriteLine("Введите значение а.");
+//a = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Введите значение b.");
+//b = Convert.ToInt32(Console.ReadLine());
+
+//int c = a;
+
+//a = b;
+//b = c;
+
+//Console.WriteLine($"a = (a), b = (b)");
+
+using System;
+
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+a = a - b;
+b = a + b;
+a = b - a;
+
+Console.WriteLine()
+
